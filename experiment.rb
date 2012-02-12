c6240da91048131fc61862fad2b271ea477f4731
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-12 21:18:19 +0100

    Alien moving
    The alien is moving around.

--- experiment.rb
+++ experiment.rb
@@ -4,8 +4,12 @@ require 'bundler/setup'
 require 'rubygame'
 
 class Alien
-  def initialize
+  def initialize(maximumX=640, maximumY=480, dx=1, dy=1)
     @surface = Rubygame::Surface.load("image/alien.png")
+    @maxX = maximumX
+    @maxY = maximumY
+    @dx = dx
+    @dy = dy
     @x = 10
     @y = 10
   end
@@ -13,19 +17,26 @@ class Alien
   def blit(screen)
     @surface.blit(screen,[@x, @y])
   end
-  
+
+  def move()
+    @x += @dx
+    @y += @dy
+    if (@x < 0 || @maxX < @x)
+      @dx *= -1
+    end
+    if (@y < 0 || @maxY < @y)
+      @dy *= -1
+    end
+  end
 end
 
 Rubygame.init
 screen = Rubygame::Screen.new [640,480]
 queue = Rubygame::EventQueue.new
 
-screen.fill [255,255,0]
 screen.update
 
 alien = Alien.new
-alien.blit(screen)
-screen.update
 
 loop do
   queue.each do |event|
@@ -34,4 +45,8 @@ loop do
       exit
     end
   end
+  screen.fill [255,255,0]
+  alien.move
+  alien.blit(screen)
+  screen.update
 end
